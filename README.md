bad0bb72ed0800aaa61f0f967f364591145a55c9
Quamir Joyner <qj_joy@yahoo.com>
2022-07-31 04:13:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Udemy Course 
+https://www.udemy.com/course/pwa-with-vuejs-quasar-firebase/
+Instructor 
+Danny Connell
+
+A PWA instagram style Web app bulit with Vuejs, Firebase NodeJs and Quasar FrameWork
+
+#  quasargram (quasargram)
+A Quasar Project
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://v1.quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white test-grey-10" bordered>
+      <q-toolbar>     
+        <q-toolbar-title class="text-grand-hotel ">Quasargram</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white" bordered>
+        <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+          <q-route-tab to="/" icon="eva-home-outline"/>
+          <q-route-tab to="/camera" icon="eva-camera-outline"/>
+        </q-tabs>
+    </q-footer>
+
+  
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .q-toolbar__title{
+    text-align: center;
+    color: black;
+    font-size: 30px;
+  }
+  .q-footer{
+    .q-tab__icon{
+      font-size: 30px;
+    }
+  }
+</style>
