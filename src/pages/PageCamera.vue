aee64aa9a41f9f037c9ab42f272b03ecd93541b0
Quamir Joyner <qj_joy@yahoo.com>
2022-08-01 15:16:42 -0400

    added Native Device Features to Camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,18 +1,55 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img
+      <video
+        v-show="!imageCaptured"
+        ref="video"
         class="full-width full-height"
-        src="https://scontent-iad3-1.xx.fbcdn.net/v/t39.30808-6/282560435_5348897808508433_1888507257501302857_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=_Bs313OjgTUAX_AU5yR&_nc_ht=scontent-iad3-1.xx&oh=00_AT8dY-z1GToFyNY_EEE1FkdvjxnEplr0RP6kephdYjzbFQ&oe=62EAC787"
+        autoplay
+      />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
       />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="black" icon="eva-camera" size="lg" />
+      <q-btn
+        v-if="hasCameraSupport"
+        round
+        @click="captureImage"
+        color="black"
+        icon="eva-camera"
+        size="lg"
+      />
+      <q-file
+        @input="captureImageFallback"
+        v-else
+        label="Choose an image"
+        accept="image/*"
+        outlined
+        v-model="imageUpload"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.caption" class="col col-sm-6" label="Caption" dense />
+        <q-input
+          v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
       </div>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.location" class="col col-sm-6" label="location" dense>
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="location"
+          dense
+        >
           <template v-slot:append>
             <q-btn round dense flat icon="eva-navigation-2-outline"></q-btn>
           </template>
@@ -26,18 +63,102 @@
 </template>
 
 <script>
-import { uid } from 'quasar';
+import { uid } from "quasar";
+import("md-gum-polyfill");
 export default {
   name: "PageCamera",
-  data(){
-    return{
-      post:{
+  data() {
+    return {
+      post: {
         id: uid(),
-        caption: '',
-        location: '',
+        caption: "",
+        location: "",
         photo: null,
-        date: Date.now()
+        date: Date.now(),
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+    };
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream;
+        })
+        .catch((error) => {
+          this.hasCameraSupport = false;
+        });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    captureImageFallback(file) {
+      this.post.photo = file;
+
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext('2d');
+
+      const render = new FileReader();
+      render.onload = (event) => {
+        const img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = event.target.result;
+      };
+      render.readAsDataURL(file);
+    },
+    disableCamera(){
+      this.$refs.video.srcObject.getVideoTracks().forEach(track =>{
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
       }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    },
+  },
+  mounted() {
+    this.initCamera();
+  },
+  beforeDestroy(){
+    if (this.hasCameraSupport){
+      this.disableCamera();
     }
   }
 };
@@ -45,7 +166,6 @@ export default {
 
 <style lang="scss" scoped>
 .camera-frame {
-  height: 370px;
   width: 100%;
   border: 2px solid $grey-10;
   border-radius: 10px;
